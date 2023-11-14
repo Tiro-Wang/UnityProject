b5e4d586eb0df37004cea55131c9a85663494ae5
Tiro-Wang <141929726+Tiro-Wang@users.noreply.github.com>
2023-11-14 22:56:22 +0800

    The first operation was a bit strenuous

---
+++ Assets/SecondTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("加点乱七八糟的东西");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
