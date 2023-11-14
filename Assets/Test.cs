949a3437388be989f70d402337e833780d660d1b
Tiro-Wang <141929726+Tiro-Wang@users.noreply.github.com>
2023-11-14 22:50:12 +0800

    提交测试

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int aa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aa = 555;
+        Debug.Log(aa);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
